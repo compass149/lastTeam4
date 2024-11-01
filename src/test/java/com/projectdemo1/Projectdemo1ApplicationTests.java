e65d53829d5127939fe560619b8264a3d6b6b9d8
GRACE\Baby <graceyong1020@gmail.com>
2024-10-31 23:40:53 -0700

    commit

---
+++ src/test/java/com/projectdemo1/Projectdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
