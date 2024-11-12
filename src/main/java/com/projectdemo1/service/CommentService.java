becbd2685cabaf339aeb4e273975d2ce3f57f585
DESKTOP-FDCFACG\admin <dlscks112@naver.com>
2024-11-12 11:22:17 +0900

    admin 페이지

--- src/main/java/com/projectdemo1/service/CommentService.java
+++ src/main/java/com/projectdemo1/service/CommentService.java
@@ -13,7 +13,10 @@ public interface CommentService {
 //    public List<Comment> getComments(Long boardId);
 //    public void deleteComment(Long id);
  Comment createComment(Long boardId, CommentDTO commentDTO);
-
+    void register(Comment comment); // 댓글 작성
+    List<Comment> listByBoard(Long bno); // 특정 게시글의 댓글 목록 조회
+    void modify(Comment comment); // 댓글 수정
+    Comment findById(Long cno); // 댓글 조회
      List<Comment> getComments(Long boardId);
      void deleteComment(Long id);
     Long register(CommentDTO commentDTO);
